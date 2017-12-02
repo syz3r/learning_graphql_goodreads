84cb146e3ed2e338a0788569c7155fcdbd75faa5
Sushil <sushilsingh2005@gmail.com>
2017-12-03 02:53:57 +0530

    finished

--- schema.js
+++ schema.js
@@ -2,10 +2,71 @@
 // secret: Du3Bu2EUZ6mZIkqPzvtjXPGvPpQqcxsgYRCx1XA8U
 const fetch = require('node-fetch')
 const util = require('util')
+const {
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLInt,
+    GraphQLString,
+    GraphQLList
+} = require('graphql')
 const parseXML = util.promisify(require('xml2js').parseString)
 
-const x = fetch('https://www.goodreads.com/author/show/18541?format=xml&key=qS6uGMXwUOXQOQZllUtVDQ')
-.then(res => res.text())
-.then(parseXML)
 
-x
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: '...',
+
+    fields: () => ({
+        title: {
+            type: GraphQLString,
+            resolve: xml => 
+            xml.title[0]
+            //console.log(JSON.stringify(xml, null, 2))
+        },
+        isbn: {
+            type: GraphQLString,
+            resolve: xml => 
+            xml.isbn[0]
+        }
+    })
+})
+
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: '...',
+
+    fields: () => ({
+        name: {
+            type: GraphQLString,
+            resolve: xml => 
+            xml.GoodreadsResponse.author[0].name[0]
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: xml =>
+            xml.GoodreadsResponse.author[0].books[0].book
+        }
+    })
+})
+
+module.exports = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'Query',
+        description: '...',
+
+        fields: () => ({
+            author: {
+                type: AuthorType,
+                args: {
+                  id: { type: GraphQLInt } 
+                },
+                resolve: (root, args) => fetch(`
+                https://www.goodreads.com/author/show/${args.id}?format=xml&key=qS6uGMXwUOXQOQZllUtVDQ`
+            )
+                .then(res => res.text())
+                .then(xml => parseXML(xml, {trim: true})) 
+            }
+        })
+    })
+})
